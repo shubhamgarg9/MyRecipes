3c9b2b8a801817207fad414524a52c849c877e49
shubhamgarg9 <shubhamrad9@gmail.com>
2017-09-18 00:59:02 +0530

    new recipe added

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -4,4 +4,31 @@ class RecipesController < ApplicationController
 		@recipes = Recipe.all
 	end
 
+	def show
+		@recipe = Recipe.find(params[:id])
+	end
+
+	def new
+		@recipe = Recipe.new
+	end
+
+	def create
+		@recipe = Recipe.new(recipe_params)
+		@recipe.chef = Chef.find(2)
+
+		if @recipe.save
+			flash[:success] = "your recipe was created successfully"
+			redirect_to recipes_path
+		else
+			render :new
+		end
+	end
+
+
+	private
+
+		def recipe_params
+			params.require(:recipe).permit(:name, :text, :description)
+		end
+
 end
