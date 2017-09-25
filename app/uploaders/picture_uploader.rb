692370a54fd053cff6d1388ae296019873f36c6e
shubhamgarg9 <shubhamrad9@gmail.com>
2017-09-25 10:48:37 +0530

    auth competed 2

--- app/uploaders/picture_uploader.rb
+++ app/uploaders/picture_uploader.rb
@@ -44,7 +44,6 @@ class PictureUploader < CarrierWave::Uploader::Base
   # Override the filename of the uploaded files:
   # Avoid using model.id or version_name here, see uploader/store.rb for details.
   # def filename
-  #   "something.jpg" if original_filename
+#   "something.jpg" if original_filename
   # end
-
 end
